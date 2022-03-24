99c32b11a4c044e80926619731d91a85528a5dbe
dddrop <go@dddrop.dev>
2022-03-24 23:36:16 +0900

    Fix library export

--- lib/filterable_logger.dart
+++ lib/filterable_logger.dart
@@ -1,7 +1,4 @@
 library filterable_logger;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/filterable_logger.dart';
+export 'src/log_record.dart';
